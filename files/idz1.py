9aaf088cb72de33f318595a42f65ccda37585119
Timofej8971 <78484537+Timofej8971@users.noreply.github.com>
2022-04-14 12:14:34 +0300

    Update idz1.py

--- files/idz1.py
+++ files/idz1.py
@@ -8,36 +8,38 @@
 '''
 
 
+def make_class(first, second, third):
+    if first > second:
+        raise ValueError()
+    else:
+        return MyClass(first, second, third)
+
+
 class MyClass:
 
     def __init__(self, first=0, second=0, third=0):
         self.first = float(first)
         self.second = float(second)
         self.third = float(third)
+        self.otvet = ""
 
     def read(self):
-        first = input("Введите начало диапазона: ")
-        second = input("Введите конец диапазона: ")
-        third = input("Введите число для проверки принадлежности к диапазону: ")
-
-        self.first = float(first)
-        self.second = float(second)
-        self.third = float(third)
+        self.first = float(input("Введите начало диапазона: "))
+        self.second = float(input("Введите конец диапазона: "))
+        self.third = float(input("Введите число для проверки принадлежности к диапазону: "))
 
     def display(self):
-        print(f"Указанное число: {rangecheck(self)}")
+        print(f"Указанное число: {self.otvet}")
 
-
-def rangecheck(self):
-    if self.first < self.second and self.first < self.third <self.second:
-        return "внутри диапозона"
-    elif self.first >= self.second:
-        raise ValueError("Неверный диапозон")
-    else:
-        return "за пределами диапозона"
+    def rangecheck(self):
+        if self.first < self.second and self.first < self.third < self.second:
+            self.otvet = "внутри диапозона"
+        else:
+            self.otvet = "за пределами диапозона"
 
 
 if __name__ == '__main__':
-    work = MyClass()
-    work.read()
+    work = make_class(1, 5, 7)
+    work.rangecheck()
     work.display()
+    
